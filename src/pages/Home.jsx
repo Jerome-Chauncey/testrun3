a2f5e397895773543d2b5dcd8c0d756d228bb1cd
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-21 01:35:58 +0300

    Adds:Disclaimer

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";  
+import Discalimer from "../components/Disclaimer";
+
+function Home(){
+    return(
+        <div>
+            <Discalimer/>
+        </div>
+    )
+}
+
+export default Home; 
